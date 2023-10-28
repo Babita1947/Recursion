33c81da5c8c997734dd174cc6b1ecae86e95159b
Babita1947 <130829271+Babita1947@users.noreply.github.com>
2023-10-28 06:54:00 +0530

    added Questions.cpp file

--- Questions.cpp
+++ Questions.cpp
@@ -15,12 +15,18 @@ void reverse(int);
 int sumN(int);
 int sum_oddN(int);
 int sum_evenN(int);
+int sum_digits(int);
+int HCF(int,int);
+void printfibo(int);
+int fibo(int);
+int countDigits(int n);
+float power(float,float);
 using namespace std;
 int main()
 {
-   int k;
-   k=sum_oddN(3);
-   cout<<"Sum = "<<k;
+   float k;
+   k=power(5,-3);
+   cout<<"Power = "<<k;
    return 0;
 }
 int sum(int n)
@@ -172,4 +178,72 @@ int sum_evenN(int n)
         return s;
     }
     
+}
+
+// 16. Sum of digits of a given numbers.
+int sum_digits(int n)
+{
+    int s;
+    if(n>0)
+    {
+        s=n%10 + sum_digits(n/10);
+        return s;
+    }
+}
+
+// 17. Calculate HCP of two given number.
+int HCF(int a,int b)
+{
+    if(a>b)
+    {
+        if(a%b==0)
+           return b;
+        else
+           return HCF(a%b,b);
+    }
+    else
+    {
+        if(b%a==0)
+           return a;
+        else
+           return HCF(b%a,a);
+    }
+}
+
+// 18. print fibonacci series
+void printfibo(int n)
+{
+    if(n==0)
+      cout<<n<<" ";
+    else
+    {
+      printfibo(n-1);
+      cout<<fibo(n)<<" ";
+    }
+    
+}
+int fibo(int n)
+{
+    if(n==0 || n==1)
+       return n;
+    return fibo(n-1)+fibo(n-2);
+}
+
+// 19. count digits
+int countDigits(int n)
+{
+    if(n==0)
+      return 0;
+    return countDigits(n/10)+1;
+}
+
+// power of a given number.
+float power(float x,float y)
+{
+    if(y==0)
+      return 1;
+    if(y>0)
+      return x*power(x,y-1);
+    if(y<0)
+      return (power(x,y+1))/x;
 }
